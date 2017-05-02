290feb0aea67fd31992ac1d45a76b41813f6253e
Isshu <isshu_araki@hotmail.com>
2017-05-02 18:09:08 +0200

    Did something

--- A4.c
+++ A4.c
@@ -30,6 +30,7 @@ int main(int argc, char* argv[], char* envp[]){
   char* args[100];
   char* paths[100];
   char* env_path;
+  char* my_path;
   char* line;
 
   env_path = getenv("SET_PATH");  //SET_PATH muss vor Programmausführung gesetzt werden.
@@ -41,23 +42,32 @@ int main(int argc, char* argv[], char* envp[]){
   split_args(paths, env_path, ":");
 
   while(1){
-    line = readline("My_shell:$");
+    line = readline("My_shell:$ ");
     split_args(args, line, " ");
-    /*Erzeuge neuen Prozess*/
-    pid_t pid = fork();
-    int status;
 
-    if(pid>=0){ //fork erfolgreich
-      if(pid==0){ //Kindprozess
-        return execv(args[0], args+1);
+    int index = 0;
+
+    while(paths[index]!=NULL){
+      /*Erzeuge neuen Prozess*/
+      pid_t pid = fork();
+      strcpy(my_path, paths[index]);
+      strcat(my_path, "/");
+      strcat(my_path, args[0]);
+      int status;
+
+      if(pid>=0){ //fork erfolgreich
+        if(pid==0){ //Kindprozess
+          return execv(my_path, args+1);
+        }
+        else{ //Elternprozess
+          wait(&status); //Warte darauf, dass Kindprozess sich beendet und speichere Exit-Code
+          printf("Exit code: %d\n", WEXITSTATUS(status));
+        }
       }
-      else{ //Elternprozess
-        wait(&status); //Warte darauf, dass Kindprozess sich beendet und speichere Exit-Code
+      else{
+        printf("Failed\n");
+        return 1;
       }
-    }
-    else{
-      printf("Failed\n");
-      return 1;
     }
 
     free(line);
